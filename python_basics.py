aee33a949ad840e7763409e4defc5b55e47d5bb4
Jabir-Srj <jabirsrj8@protonmail.com>
2021-10-04 21:40:45 +0300

    Latest

---
+++ python_basics.py
@@ -0,0 +1,32 @@
+#Python Basics#
+# Note: Remove the (#) in order to execute the code.
+
+##Defining a variable##
+# my_var = 10  # type integer
+# my_var = "Hello"  # type string
+# my_var = True  # type boolean
+
+##Print Statement##
+# print("I am already writing code and enjoying this course!")
+
+##User Input##
+# input("This is a text copier, type anything and it will copy it:")
+
+##Saving the input in a variable##
+# I_ask = input("This is text copier, type anything and it will copy it: ")
+# print(I_ask)
+
+##Memory location## Checking where info is stored in the memory
+# id()
+
+                ### Basic Keywords ###
+# False       await      else       import     pass
+# None       break      except     in         raise
+# True       class      finally    is         return
+# and        continue   for        lambda     try
+# as         def        from       nonlocal   while
+# assert     del        global     not        with
+# async      elif       if         or         yield
+
+## Data Types ##
+# https://docs.python.org/3/library/stdtypes.html
