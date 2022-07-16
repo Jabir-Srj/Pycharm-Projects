1c896789b6ed113082f02bb021f012275cd699d6
fattiejb <jabirsrj8@protonmail.com>
2022-07-16 14:44:35 +0300

    Latest

---
+++ test/test1.py
@@ -0,0 +1,8 @@
+x = 5
+if x > 5:
+	print("x is greater than 5")
+	print(x * 2)
+elif x == 5:
+	print("x is equal to 5")	
+else:
+	print("x is NOT greater than 5")
---
+++ test/test2.py
@@ -0,0 +1,7 @@
+x = 100 
+if x == 5 and type(x) is int:
+	print("x is equal to 5 and x is an integer")
+	print(x)
+elif x == 10 and type(x) is int:
+	print("x is an integer but it is not equal to 5")
+	print(x)
